2acb4174c18168c9f4c80628363e0ddcb204c407
marvinick <marviluw@gmail.com>
2015-08-21 09:14:52 -0700

    this commit will add starting app.php

---
+++ app/app.php
@@ -0,0 +1,28 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Stylist.php";
+
+    $app = new Silex\Application();
+
+    $server = 'mysql:host=localhost;dbname=food';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+      'twig.path' => array (
+           __DIR__.'/../views'
+      )
+    ));
+
+
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('index.html.twig', array('cuisines' => Cuisine::getAll()));
+    });
+
+    return $app;
+
+?>
+
+
+
